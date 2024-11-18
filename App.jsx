76fb27e35ad684075d36f3c37cb964d7cc975ca5
Codey Gallup <codey.gallup@gmail.com>
2024-11-18 12:06:18 -0700

    Adding component with props

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+// Import data and WeatherCard here
+import cities from "./data";
+import WeatherCard from "./components/WeatherCard";
+
+function App() {
+  return (
+    <>
+      <h1 className="title">REACTIVE WEATHER</h1>
+      <h3 className="subtitle">Up to the minute weather news</h3>
+      <div className="app">
+        {/* Render components here */}
+        {cities.map((city, index) => (
+          <WeatherCard
+            key={index}
+            city={city.city}
+            temp={city.temperature}
+            forecast={city.forecast}
+          />
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
