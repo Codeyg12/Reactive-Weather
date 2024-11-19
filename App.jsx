327fdff46f5c4d687a5529fbec00ba53fe99d7fb
Codey Gallup <codey.gallup@gmail.com>
2024-11-19 10:13:45 -0700

    Created user input and error handling

--- App.jsx
+++ App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import cities from "./data";
 import WeatherCard from "./components/WeatherCard";
 import Location from "./components/Location";
+import Form from "./components/Form";
 
 function App() {
   const [location, setLocation] = useState("Rio de Janeiro");
@@ -11,8 +12,9 @@ function App() {
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
+      <Form setLocation={setLocation} />
       <div className="app">
-        <Location data={cities} location={location} setLocation={setLocation} />
+        <Location data={cities} location={location} />
         {/* Render components here */}
         {cities.map((city, index) => (
           <WeatherCard
--- components/Location.jsx
+++ components/Location.jsx
@@ -1,28 +1,52 @@
+import { useEffect, useState } from "react";
 import icon from "../utils/helper";
 
-const Location = ({ data, location, setLocation }) => {
-  const currentCity = () => {
-    const city = data.find((el) => el.city === location);
-    return city;
-  };
+const Location = ({ data, location }) => {
+  const [city, setCity] = useState(location);
+  const [errorMsg, setErrorMsg] = useState("Searching for locations...");
+
+  useEffect(() => {
+    const city = data.find(
+      (el) => el.city.toLowerCase() === location.toLowerCase()
+    );
+    if (city === undefined) {
+      setErrorMsg(`The weather for ${location} is not available`);
+      return;
+    }
+    setCity(city);
+    setErrorMsg(null);
+  }, [location]);
+
+  console.log(city);
 
   return (
     <div className="card">
-      <h3 className="card-title">Your Location's Weather</h3>
-      <div className="img-container">
-        <img
-          className="card-img-top"
-          src={icon(currentCity().forecast)}
-          alt="Card image cap"
-          id="icon"
-        />
-      </div>
-      <div className="card-body">
-        <h4 className="card-title">
-          The weather in {currentCity().city} is {currentCity().temperature}
-        </h4>
-        <h5 className="card-text">It is {currentCity().forecast} today</h5>
-      </div>
+      {errorMsg ? (
+        <>
+          <h3 className="card-title text-center">Location not found</h3>
+          <div className="card-body">
+            <h4 className="card-title">{errorMsg}</h4>
+          </div>
+        </>
+      ) : (
+        <>
+          <h3 className="card-title">Your Location's Weather</h3>
+          <div className="img-container">
+            <img
+              className="card-img-top"
+              src={icon(city.forecast)}
+              alt="Card image cap"
+              id="icon"
+            />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title">
+              The weather in {city.city} is {city.temperature}
+            </h4>
+            <h5 className="card-text">It is {city.forecast} today</h5>
+          </div>
+        </>
+      )}
     </div>
   );
 };
