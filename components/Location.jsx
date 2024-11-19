011ec344a8166a06624cee919bc224fd4c9921a4
Codey Gallup <codey.gallup@gmail.com>
2024-11-19 15:14:38 -0700

    Adding all weather button for generic data, new icons, and API

--- components/Location.jsx
+++ components/Location.jsx
@@ -2,51 +2,25 @@ import { useEffect, useState } from "react";
 import icon from "../utils/helper";
 
 const Location = ({ data, location }) => {
-  const [city, setCity] = useState(location);
-  const [errorMsg, setErrorMsg] = useState("Searching for locations...");
-
-  useEffect(() => {
-    const city = data.find(
-      (el) => el.city.toLowerCase() === location.toLowerCase()
-    );
-    if (city === undefined) {
-      setErrorMsg(`The weather for ${location} is not available`);
-      return;
-    }
-    setCity(city);
-    setErrorMsg(null);
-  }, [location]);
-
-  console.log(city);
+  const { name, temp, forecast } = location;
 
   return (
     <div className="card">
-      {errorMsg ? (
-        <>
-          <h3 className="card-title text-center">Location not found</h3>
-          <div className="card-body">
-            <h4 className="card-title">{errorMsg}</h4>
-          </div>
-        </>
-      ) : (
-        <>
-          <h3 className="card-title">Your Location's Weather</h3>
-          <div className="img-container">
-            <img
-              className="card-img-top"
-              src={icon(city.forecast)}
-              alt="Card image cap"
-              id="icon"
-            />
-          </div>
-          <div className="card-body">
-            <h4 className="card-title">
-              The weather in {city.city} is {city.temperature}
-            </h4>
-            <h5 className="card-text">It is {city.forecast} today</h5>
-          </div>
-        </>
-      )}
+      <h3 className="card-title">Your Location's Weather</h3>
+      <div className="img-container">
+        <img
+          className="card-img-top"
+          src={icon(forecast)}
+          alt="Card image cap"
+          id="icon"
+        />
+      </div>
+      <div className="card-body">
+        <h4 className="card-title">
+          The weather in {name} is {temp}°F
+        </h4>
+        <h5 className="card-text">It is {forecast} today</h5>
+      </div>
     </div>
   );
 };
